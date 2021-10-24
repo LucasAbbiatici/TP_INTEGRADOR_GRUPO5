03232a1df0dcd2ab0c247f42c60161d0238a8380
LucasAbbiatici <lucas.abbiatici@gmail.com>
2021-10-24 20:49:05 -0300

    pantalla mapa + floating action buttons

--- app/src/main/java/com/example/tp_integrador_grupo5/MainActivity.java
+++ app/src/main/java/com/example/tp_integrador_grupo5/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.tp_integrador_grupo5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void redirec_register(View view){
+        Intent i = new Intent(this, RegisterActivity.class);
+        startActivity(i);
+    }
+
+    public void ingresar(View view){
+        Intent i = new Intent(this, MapsActivity.class);
+        startActivity(i);
+    }
+
 }
