a2fb2652c7e8c66a418324aba2daa3aff2a937ec
LucasAbbiatici <lucas.abbiatici@gmail.com>
2021-11-03 01:56:23 -0300

    permisos de ubicación (pantalla login)

--- app/src/main/java/com/example/tp_integrador_grupo5/MainActivity.java
+++ app/src/main/java/com/example/tp_integrador_grupo5/MainActivity.java
@@ -1,17 +1,42 @@
 package com.example.tp_integrador_grupo5;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AlertDialog.Builder builder;
+    private AlertDialog alertDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setMessage(R.string.dialog_compartirUbicacion).setPositiveButton("ACEPTAR", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            }
+        }).setNegativeButton("CANCELAR", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        }).setTitle("Compartir Ubicación");
+
+        alertDialog = builder.create();
+
     }
 
     public void redirec_register(View view){
@@ -20,8 +45,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void ingresar(View view){
-        Intent i = new Intent(this, MapsActivity.class);
-        startActivity(i);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            alertDialog.show();
+        } else {
+            Intent i = new Intent(this, MapsActivity.class);
+            startActivity(i);
+        }
+
     }
 
 }
