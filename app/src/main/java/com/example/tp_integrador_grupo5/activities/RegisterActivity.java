e9e6019a50502b28ae19d9cc3850d55c8dd64c64
FedeAragon <fede-thiago@live.com.ar>
2021-11-03 22:05:15 -0300

    Creacion DataUserActivity para insertar usuarios que se estan registrando

--- app/src/main/java/com/example/tp_integrador_grupo5/activities/RegisterActivity.java
+++ app/src/main/java/com/example/tp_integrador_grupo5/activities/RegisterActivity.java
@@ -3,14 +3,60 @@ package com.example.tp_integrador_grupo5.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.tp_integrador_grupo5.R;
+import com.example.tp_integrador_grupo5.conexion.Asynctask.DataUserActivity;
+import com.example.tp_integrador_grupo5.entidades.Usuario;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private EditText et_nombre;
+    private EditText et_apellido;
+    private EditText et_email2;
+    private EditText et_pass;
+    private EditText et_repetirpass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        et_nombre = (EditText) findViewById(R.id.et_nombre);
+        et_apellido = (EditText) findViewById(R.id.et_apellido);
+        et_email2 = (EditText) findViewById(R.id.et_email2);
+        et_pass = (EditText) findViewById(R.id.et_pass);
+        et_repetirpass = (EditText) findViewById(R.id.et_repetirpass);
+    }
+
+    //Evento del boton registrarse cuando se hace click.
+    public void RegisterOnClick(View view){
+        if (ControlIsEmpty()){
+            Toast.makeText(this, "Por favor complete todos los campos", Toast.LENGTH_LONG).show();
+        }
+        else if (!et_pass.getText().toString().equals(et_repetirpass.getText().toString())){
+            Toast.makeText(this, "Las contraseñas no coinciden", Toast.LENGTH_LONG).show();
+        }
+        else{
+            Usuario usuario = new Usuario();
+            usuario.setNombre(et_nombre.getText().toString());
+            usuario.setApellido(et_apellido.getText().toString());
+            usuario.setEmail(et_email2.getText().toString());
+            usuario.setPassword(et_pass.getText().toString());
+            DataUserActivity dataUserActivity = new DataUserActivity(usuario, this);
+            dataUserActivity.execute();
+        }
+    }
+
+    private boolean ControlIsEmpty(){
+        boolean isEmpty = false;
+        if ( et_nombre.getText().toString().isEmpty() ) isEmpty = true;
+        if ( et_apellido.getText().toString().isEmpty() ) isEmpty = true;
+        if ( et_email2.getText().toString().isEmpty() ) isEmpty = true;
+        if ( et_pass.getText().toString().isEmpty() ) isEmpty = true;
+        if ( et_repetirpass.getText().toString().isEmpty() ) isEmpty = true;
+        return isEmpty;
     }
 }
---
+++ app/src/main/java/com/example/tp_integrador_grupo5/conexion/Asynctask/DataUserActivity.java
@@ -0,0 +1,51 @@
+package com.example.tp_integrador_grupo5.conexion.Asynctask;
+
+import android.content.Context;
+import android.os.AsyncTask;
+
+import com.example.tp_integrador_grupo5.conexion.DataDB;
+import com.example.tp_integrador_grupo5.entidades.Usuario;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class DataUserActivity extends AsyncTask<String, Void, String> {
+
+    private Usuario usuario;
+    private Context context;
+    private static String result2;
+
+    public DataUserActivity(Usuario usuario, Context context) {
+        this.usuario = usuario;
+        this.context = context;
+    }
+
+    @Override
+    protected String doInBackground(String... strings) {
+        String response = "";
+        try {
+            result2 = " ";
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection(DataDB.urlMySQL, DataDB.user, DataDB.pass);
+            PreparedStatement statement = con.prepareStatement("INSERT INTO Usuarios (Nombre, Apellido, Email, " +
+                    "Password) VALUES (? ,? ,? ,? )");
+            statement.setString(1, usuario.getNombre());
+            statement.setString(2, usuario.getApellido());
+            statement.setString(3, usuario.getEmail());
+            statement.setString(4, usuario.getPassword());
+
+            statement.execute();
+
+            response = "Conexion exitosa";
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            result2 = "Conexion no exitosa";
+        }
+
+        return response;
+    }
+}
