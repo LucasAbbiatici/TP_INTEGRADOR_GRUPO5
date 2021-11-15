6f18f3aad23f9c7f7e495bef444bcbe1588d15d1
LucasAbbiatici <lucas.abbiatici@gmail.com>
2021-11-15 18:18:43 -0300

    cambio base

--- app/src/main/java/com/example/tp_integrador_grupo5/conexion/DataDB.java
+++ app/src/main/java/com/example/tp_integrador_grupo5/conexion/DataDB.java
@@ -5,9 +5,9 @@ public class DataDB {
     //Información de la BD
     public static String host="sql10.freesqldatabase.com";
     public static String port="3306";
-    public static String nameBD="sql10449713";
-    public static String user="sql10449713";
-    public static String pass="zLkZx2yT2b";
+    public static String nameBD="sql10451383";
+    public static String user="sql10451383";
+    public static String pass="7u6fCjZumS";
 
     //Información para la conexion
     public static String urlMySQL = "jdbc:mysql://" + host + ":" + port + "/" + nameBD;
